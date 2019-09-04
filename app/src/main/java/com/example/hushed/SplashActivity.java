e644ef1e5bf00165390a7462e7e49fe345387b98
Wes <wesley.kramer@live.com>
2019-09-04 09:57:15 -0600

    Splash activity
    Addition of splash page

---
+++ app/src/main/java/com/example/hushed/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.example.wesley.finalproject;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+    private Handler mWaitHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mWaitHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                //The following code will execute after the 5 seconds.
+
+                try {
+
+                    //Go to next page i.e, start the next activity.
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+
+                    //Let's Finish Splash Activity since we don't want to show this when user press back button.
+                    finish();
+                } catch (Exception ignored) {
+                    ignored.printStackTrace();
+                }
+            }
+        }, 4000);  // Give a 4 seconds delay.
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        //Remove all the callbacks otherwise navigation will execute even after activity is killed or closed.
+        mWaitHandler.removeCallbacksAndMessages(null);
+    }
+}
