0e2c41214da71428684c24445f57b7262ea5a9ec
Francisco Duran <Franciscoduran93@gmail.com>
2019-09-04 13:03:24 -0600

    added fake senders

--- app/src/main/java/com/example/hushed/DataSource.kt
+++ app/src/main/java/com/example/hushed/DataSource.kt
@@ -10,36 +10,49 @@ class DataSource {
             val list = ArrayList<Messages>()
             list.add(
                 Messages(
+                    sender = "Friend Unit 1",
                     message = "Hey there!"
                 )
             )
             list.add(
                 Messages(
+                    sender = "Parental Unit 1",
                     message = "Please call me back"
                 )
             )
             list.add(
                 Messages(
+                    sender = "Friend Unit 2",
                     message = "Just wanted to let you know..."
                 )
             )
             list.add(
                 Messages(
+                    sender = "Sibling Unit 1",
+                    message = "Please don't tell Parental Unit 1 about this"
+                )
+            )
+            list.add(
+                Messages(
+                    sender = "Friend Unit 3",
                     message = "Bruh!"
                 )
             )
             list.add(
                 Messages(
+                    sender = "Group Member 1",
                     message = "Need the report to be finished soon"
                 )
             )
             list.add(
                 Messages(
+                    sender = "Parental Unit 2",
                     message = "See you this weekend"
                 )
             )
             list.add(
                 Messages(
+                    sender = "Gandalf the Grey",
                     message = "You Shall Not PASS!"
                 )
             )
