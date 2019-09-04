48abe651f19251b789ca2d92763b8651e14c40f7
Francisco Duran <Franciscoduran93@gmail.com>
2019-09-04 00:00:54 -0600

    RecyclerView init

--- app/src/main/java/com/example/hushed/MainActivity.kt
+++ app/src/main/java/com/example/hushed/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.hushed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var messageAdapter: MessageRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initRecyclerView()
+        addDataSet()
+    }
+
+    private fun addDataSet() {
+        val data = DataSource.createDataSet()
+        messageAdapter.submitList(data)
+    }
+
+    private fun initRecyclerView(){
+        recyclerViewHome.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            messageAdapter = MessageRecyclerAdapter()
+            adapter = messageAdapter
+        }
     }
 }
