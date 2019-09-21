345bf5b83803e4576e28d86f60eadd65732395ae
Francisco Duran <Franciscoduran93@gmail.com>
2019-09-21 16:04:52 -0600

    IMEI used for addressing
    The IMEI is now used for addressing, instead of a dummy address. However more discussion is needed to know if we want to use this

--- app/src/main/java/com/example/hushed/MainActivity.kt
+++ app/src/main/java/com/example/hushed/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telephony.TelephonyManager
 import android.util.Log
-import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,7 +15,7 @@ import com.google.firebase.firestore.SetOptions
 
 class MainActivity : AppCompatActivity() {
     var dummyAddress = "Brian"
-    val dummyData = hashMapOf(
+    var dummyData = hashMapOf(
         "Nicolas" to hashMapOf("Francisco" to "Wassup", "Wes" to "Howdy"),
         "Wes" to hashMapOf("Francisco" to "Yoo", "Nicolas" to "What it do???"),
         dummyAddress to hashMapOf("Francisco" to "Wasssuuup", "Nicolas" to "Helloooo")
@@ -29,6 +28,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val displayIMEI = retrieveImei()
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_PHONE_STATE)) {
@@ -43,21 +44,18 @@ class MainActivity : AppCompatActivity() {
 
         // SETTINGS BUTTON **************************************************************************
         button_settings.setOnClickListener {
+            println("SETTINGS BUTTON")
             Log.i("Button","Click: button_settings")
         }
 
         // ABOUT BUTTON **************************************************************************
         button_about.setOnClickListener {
-            try{
-                val tm = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-                val IMEI = tm.getImei()
-                if (IMEI != null)
-                    Toast.makeText(this, "IMEI number: " + IMEI,
-                        Toast.LENGTH_LONG).show()
 
-            }catch (ex:Exception){
-                Log.i("", "There was a problem")
+            if (displayIMEI != null) {
+                this.dummyAddress = displayIMEI
             }
+
+            this.dummyData = hashMapOf(dummyAddress to hashMapOf("Brian" to "This is from the emulator", "Nicholas" to "...Are you getting sick?"))
         }
 
         // Dummy BUTTON **************************************************************************
@@ -70,19 +68,19 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Dummy BUTTON **************************************************************************
-        button_dummy.setOnClickListener {
-            val dummyData = hashMapOf(
-                "Nicolas" to hashMapOf("Francisco" to "Wassup", "Wes" to "Howdy"),
-                "Wes" to hashMapOf("Francisco" to "Yoo", "Nicolas" to "What it do???"),
-                dummyAddress to hashMapOf("Francisco" to "Yoo", "Nicolas" to "What it do???")
-            )
-
-            db.set(dummyData, SetOptions.merge())
-                .addOnSuccessListener { Log.d("Firebase", "DocumentSnapshot successfully written!") }
-                .addOnFailureListener { e -> Log.w("Firebase", "Error writing document", e) }
-
-            Log.i("Button","Click: button_dummy")
-        }
+//        button_dummy.setOnClickListener {
+//            val dummyData = hashMapOf(
+//                "Nicolas" to hashMapOf("Francisco" to "Wassup", "Wes" to "Howdy"),
+//                "Wes" to hashMapOf("Francisco" to "Yoo", "Nicolas" to "What it do???"),
+//                dummyAddress to hashMapOf("Francisco" to "Yoo", "Nicolas" to "What it do???")
+//            )
+//
+//            db.set(dummyData, SetOptions.merge())
+//                .addOnSuccessListener { Log.d("Firebase", "DocumentSnapshot successfully written!") }
+//                .addOnFailureListener { e -> Log.w("Firebase", "Error writing document", e) }
+//
+//            Log.i("Button","Click: button_dummy")
+//        }
 
         // Retrieve BUTTON **************************************************************************
         button_retrieve.setOnClickListener {
@@ -98,5 +96,16 @@ class MainActivity : AppCompatActivity() {
 
             Log.i("Button","Click: button_retrieve")
         }
+    }
+
+    fun retrieveImei(): String? {
+        try{
+            val tm = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            val test = tm.getImei()
+            return test
+        }catch (ex:Exception){
+            Log.i("", "There was a problem")
+        }
+        return "ERROR" // not sure what this should return
     }
 }
