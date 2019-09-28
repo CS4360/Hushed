aded52a45382e919cd3883e704b8cc7fa3a4530c
Francisco Duran <Franciscoduran93@gmail.com>
2019-09-27 21:44:30 -0600

    Testing activity sender message

--- app/src/main/java/com/example/hushed/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/hushed/DisplayMessageActivity.kt
@@ -4,18 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_display_message.*
+import kotlinx.android.synthetic.main.activity_sender_message.*
 
 class DisplayMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_display_message)
+        setContentView(R.layout.activity_sender_message)
 
         val intentThatStartedThisActivity = intent
 
         if(intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
             var msg = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT)
-            texView.text = msg
+            txtMyMessage.text = msg
         }
     }
 }
