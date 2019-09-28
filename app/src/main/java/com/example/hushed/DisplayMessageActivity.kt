1069f13eaf4a5a798e67f1b03208c89035be2c67
Francisco Duran <Franciscoduran93@gmail.com>
2019-09-27 21:46:56 -0600

    Revert "Testing activity sender message"
    This reverts commit aded52a45382e919cd3883e704b8cc7fa3a4530c.

--- app/src/main/java/com/example/hushed/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/hushed/DisplayMessageActivity.kt
@@ -4,19 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_display_message.*
-import kotlinx.android.synthetic.main.activity_sender_message.*
 
 class DisplayMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sender_message)
+        setContentView(R.layout.activity_display_message)
 
         val intentThatStartedThisActivity = intent
 
         if(intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
             var msg = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT)
-            txtMyMessage.text = msg
+            texView.text = msg
         }
     }
 }
