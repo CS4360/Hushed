7b774d0548e81b9167f451db5819ef0412ebe8a9
Wes <wesley.kramer@live.com>
2019-09-28 19:24:31 -0600

    User input
    Takes user input to val messageText within DisplayMessageActivity.kt

--- app/src/main/java/com/example/hushed/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/hushed/DisplayMessageActivity.kt
@@ -2,10 +2,15 @@ package com.example.hushed
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_display_message.*
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class DisplayMessageActivity : AppCompatActivity() {
+
+    var messageText = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +21,20 @@ class DisplayMessageActivity : AppCompatActivity() {
         if(intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
             var msg = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT)
             texView.text = msg
+        }
+
+        send_button.setOnClickListener {
+            Log.i("tag", "Click: send_button Button")
+            if(editText.text.isNullOrBlank()) {
+                Toast.makeText(this@DisplayMessageActivity, "Message cannot be blank", Toast.LENGTH_LONG).show()
+                Log.i("tag", "Blank message entered")
+            } else {
+                messageText = editText.text.toString()
+                Log.i("tag", "$messageText")
+            }
+
+            editText.text.clear()
+
         }
     }
 }
