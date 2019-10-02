f14c834e62ef0522004192e259b3bb1e29d0367a
Francisco Duran <Franciscoduran93@gmail.com>
2019-10-01 20:25:18 -0600

    some clean up
    messaged recieved bubble is now shown, the send bubble also shows up but some variables need to be changed in order to show them

--- app/src/main/java/com/example/hushed/DisplayRecyclerAdapter.kt
+++ app/src/main/java/com/example/hushed/DisplayRecyclerAdapter.kt
@@ -6,50 +6,42 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hushed.models.Messages
 import kotlinx.android.synthetic.main.activity_message_sent.view.*
-//import kotlinx.android.synthetic.main.activity_messaged_received.view.*
-
-private const val VIEW_TYPE_SENT = 1
-private const val VIEW_TYPE_RECIEVED = 2
+import kotlinx.android.synthetic.main.activity_messaged_received.view.*
 
 class DisplayRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-//    private var messages: List<Messages> = ArrayList()
-    private var message: MutableList<String> = ArrayList()
+    private var messages: MutableList<Messages> = ArrayList()
+//    private var message: MutableList<String> = ArrayList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return DisplayViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.activity_message_sent, parent, false) // Sent Message Bubble
-//            LayoutInflater.from(parent.context).inflate(R.layout.activity_messaged_received, parent, false)   // Sent Received Bubble
+//            LayoutInflater.from(parent.context).inflate(R.layout.activity_message_sent, parent, false) // Sent Message Bubble
+            LayoutInflater.from(parent.context).inflate(R.layout.activity_messaged_received, parent, false)   // Sent Received Bubble
         )
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when(holder) {
             is DisplayViewHolder ->
-                holder.bind(message[position])
+                holder.bind(messages[position])
         }
     }
 
     override fun getItemCount(): Int {
-        return message.size
+        return messages.size
     }
 
-//    fun submitList(msg: List<Messages>) {
-//        messages = msg
-//    }
-
-    fun sentList(msg: String) {
-        message.add(msg)
-    }
-
-    fun submitList(msg: String) {
-        message.add(msg)
+    fun submitList(msg: String, sender: String) {
+        messages.add(Messages(
+            sender = sender,
+            message = msg
+        ))
     }
 
     class DisplayViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(msg: String) {
+        fun bind(msg: Messages) {
             // just display the other message (received)
-            itemView.txtMyMessage.text = msg
+            itemView.txtOtherMessage.text = msg.message
 
         }
     }
