ca2d3608b8fbb2fb56e724786aec11db91e73275
Francisco Duran <Franciscoduran93@gmail.com>
2019-10-01 19:31:55 -0600

    sent message test

--- app/src/main/java/com/example/hushed/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/hushed/DisplayMessageActivity.kt
@@ -1,11 +1,16 @@
 package com.example.hushed
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_display_message.*
 import kotlinx.android.synthetic.main.activity_message_chat.*
 
 class DisplayMessageActivity : AppCompatActivity() {
+
+    var messageText = ""
 
     private lateinit var displayAdapter: DisplayRecyclerAdapter
 
@@ -13,13 +18,32 @@ class DisplayMessageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message_chat)
 
+        val actionBar = supportActionBar
+        actionBar!!.title = intent.getStringExtra(EXTRA_TEXT)
+
         initRecyclerView()
         addDataSet()
+
+        btnSend.setOnClickListener{
+            Log.i("tag", "Click: send_button Button")
+            if(txtMessage.text.isNullOrBlank()) {
+                Toast.makeText(this@DisplayMessageActivity, "Message cannot be blank", Toast.LENGTH_LONG).show()
+                Log.i("tag", "Blank message entered")
+            } else {
+                messageText = txtMessage.text.toString()
+                Log.i("tag", "$messageText")
+            }
+            initRecyclerView()
+            sentDataSet(messageText)
+            txtMessage.text.clear()
+        }
+    }
+
+    private fun sentDataSet(messageText: String) {
+        displayAdapter.sentList(messageText)
     }
 
     private fun addDataSet() {
-        val actionBar = supportActionBar
-        actionBar!!.title = intent.getStringExtra(EXTRA_TEXT)
 //        val data = DataSource.getDataSet()
         val intentMsg = intent.getStringExtra(EXTRA_MESSAGE)
         displayAdapter.submitList(intentMsg)
--- app/src/main/java/com/example/hushed/DisplayRecyclerAdapter.kt
+++ app/src/main/java/com/example/hushed/DisplayRecyclerAdapter.kt
@@ -5,7 +5,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hushed.models.Messages
-import kotlinx.android.synthetic.main.activity_messaged_received.view.*
+import kotlinx.android.synthetic.main.activity_message_sent.view.*
+//import kotlinx.android.synthetic.main.activity_messaged_received.view.*
+
+private const val VIEW_TYPE_SENT = 1
+private const val VIEW_TYPE_RECIEVED = 2
 
 class DisplayRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -14,7 +18,8 @@ class DisplayRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return DisplayViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.activity_messaged_received, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.activity_message_sent, parent, false) // Sent Message Bubble
+//            LayoutInflater.from(parent.context).inflate(R.layout.activity_messaged_received, parent, false)   // Sent Received Bubble
         )
     }
 
@@ -33,6 +38,10 @@ class DisplayRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 //        messages = msg
 //    }
 
+    fun sentList(msg: String) {
+        message.add(msg)
+    }
+
     fun submitList(msg: String) {
         message.add(msg)
     }
@@ -40,7 +49,8 @@ class DisplayRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     class DisplayViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(msg: String) {
             // just display the other message (received)
-            itemView.txtOtherMessage.text = msg
+            itemView.txtMyMessage.text = msg
+
         }
     }
 }
