2e913b8eec6fc351b5c4d1fdabaf600e40b50d94
Wes <wesley.kramer@live.com>
2019-11-13 16:36:09 -0700

    Adding inflated menu to conversationsactivity

--- app/src/main/java/com/example/hushed/ConversationsActivity.kt
+++ app/src/main/java/com/example/hushed/ConversationsActivity.kt
@@ -2,6 +2,8 @@ package com.example.hushed
 
 import android.util.Log
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.content.Intent
 import android.content.Context
 
@@ -189,6 +191,24 @@ class ConversationsActivity : AppCompatActivity() {
                         Log.w("Firebase", "Error retrieving data from $partnerNickname: ", e)
                     }
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val intent = Intent(applicationContext, NicknameActivity::class.java)
+        startActivity(intent)
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
